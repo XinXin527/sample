e28912c4318db56413f36189ab960aebdb23d886
XinXin527 <48648468+XinXin527@users.noreply.github.com>
2019-06-08 17:34:44 +0800

    Add files via upload

---
+++ erweilixiangcaiyang.m
@@ -0,0 +1,65 @@
+u1=25;
+u2=25;
+sigma=0.5;
+x=[0:0.05:50];
+y=[0:0.05:50];
+[X,Y]=meshgrid(x,y);
+xy=(1/(2*pi*(sigma^2)))*exp((-((X-u1).^2+(Y-u2).^2))/(2*(sigma.^2)));
+figure(1);
+surf(X,Y,xy);
+title('原高斯函数');
+hold on;
+figure(2);
+XY=fftshift(abs(fft2(xy)));
+surf(Y,X,XY);
+title('高斯函数傅里叶变换');
+hold on;
+xyp=zeros(1001,1001);
+for i=1:1:201
+    p=5*(i-1)+1;
+    for j=1:1:201
+        q=5*(j-1)+1;
+    xyp(p,q)=1;
+    end
+end
+xypp=zeros(1001,1001);
+for i=1:1:1001
+    for j=1:1:1001
+        xypp(i,j)=xy(i,j)*xyp(i,j);
+    end
+end
+figure(3);
+surf(X,Y,xypp);
+title('采样后得到的序列');
+hold on;
+XYp=fftshift(abs(fft2(xypp)));
+figure(4);
+surf(Y,X,XYp);
+title('采样后的序列做傅里叶变换得到的结果');
+hold on;
+lvbo=zeros(1001,1001);
+for i=400:1:600
+    for j=400:1:600
+        lvbo(j,i)=25;
+    end
+end
+XYpp=zeros(1001,1001);
+for j=1:1:1001
+    for i=1:1:1001
+        XYpp(j,i)=lvbo(j,i)*XYp(j,i);
+    end
+end
+figure(5);
+surf(Y,X,XYpp);
+title('频域滤波后的结果');
+hold on;
+XX=ifftshift(abs(ifft2(XYpp)));
+figure(6);
+surf(X,Y,XX);
+title('恢复的原信号');
+figure(7);
+surf(X,Y,xy);
+hold on;
+surf(X,Y,XX);
+legend('原高斯函数','恢复的高斯函数');
+title('方便比较');
---
+++ yiweilixiangcaiyang.m
@@ -0,0 +1,53 @@
+u=25;
+sigma=0.5;
+x=0:0.05:50;
+y=(1/(sqrt(2*pi)*sigma))*exp(-(((x-u).^2)/(2*(sigma.^2))));
+figure(1);
+plot(x,y);
+title('原高斯函数');
+hold on;
+figure(2);
+Y=fftshift(abs(fft(y)));
+plot(x,Y);
+title('高斯函数傅里叶变换');
+hold on;
+xp=zeros(1,1001);
+for i=1:1:201
+    p=5*(i-1)+1;
+    xp(1,p)=1;
+end
+xpp=zeros(1,1001);
+for j=1:1:1001
+    xpp(1,j)=y(1,j)*xp(1,j);
+end
+figure(3);
+stem(x,xpp);
+title('采样后得到的序列');
+hold on;
+yp=fftshift(abs(fft(xpp)));
+figure(4);
+plot(x,yp);
+title('采样后的序列做傅里叶变换得到的结果');
+hold on;
+lvbo=zeros(1,1001);
+for i=400:1:600
+    lvbo(1,i)=5;
+end
+Yp=zeros(1,1001);
+for j=1:1:1001
+    Yp(1,j)=lvbo(1,j)*yp(1,j);
+end
+figure(5);
+plot(x,Yp);
+title('频域滤波后的结果');
+hold on;
+X=ifftshift(abs(ifft(Yp)));
+figure(6);
+plot(x,X);
+title('恢复的原信号');
+figure(7);
+plot(x,y);
+hold on;
+plot(x,X);
+legend('原高斯函数','恢复的高斯函数');
+title('方便比较');
